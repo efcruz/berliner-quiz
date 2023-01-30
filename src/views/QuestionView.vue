05f6cf3d2e13047b5d793418e49a515b9dbad5cf
efcruz <eduarda.web.dev@gmail.com>
2023-01-30 10:45:40 +0100

    Create QuestionView.vue

---
+++ src/views/QuestionView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="question">
+   <h2>This is the first question</h2>
+   <div class="options">
+      <input type="checkbox" id="optionOne" name="optionOne">
+      <label for="optionOne">This is option One</label>
+
+      <input type="checkbox" id="optionTwo" name="optionTwo">
+      <label for="optionTwo">This is option Two</label>
+    </div>
+    <input type="submit" value="Next" />
+  </div>
+</template>
